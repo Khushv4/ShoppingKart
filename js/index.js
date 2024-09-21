92b964be378c79f31202254d4468bce16e21e0cb
Khushv4 <khushalvyas21@gmail.com>
2024-09-21 17:13:18 +0530

    added basic UI of ShoppingKart

---
+++ js/index.js
@@ -0,0 +1,20 @@
+async function fetchCategories(){
+  const response = await fetch("https://fakestoreapi.com/products/categories");
+  const data = await response.json();
+  return data;
+}
+
+async function populateCategories(){
+  const categories = await fetchCategories();
+  const categoryList = document.querySelector('#categoryList');
+  categories.forEach(category => {
+    const categoryHolder = document.createElement('div');
+    const categoryLink = document.createElement('a');
+    categoryLink.href = '#';
+    categoryHolder.classList.add('category-item', 'd-flex', 'align-items-center', 'justify-content-center');
+    categoryLink.textContent = category;
+    categoryHolder.appendChild(categoryLink);// adding a tag to catgeory holder (div)
+    categoryList.appendChild(categoryHolder);  
+  });
+}
+populateCategories();
